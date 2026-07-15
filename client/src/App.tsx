af172bde7b3703ef8b710c2e55872adb0ab8f0c5
Imanuelkarl <appdeveloperemmanuel@gmail.com>
2026-07-15 12:30:02 +0100

    questionnaire

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -87,6 +87,7 @@ function App() {
                   <ProtectedRoute>
                     <AppLayout>
                       <QuestionnaireForm />
+                      
                     </AppLayout>
                   </ProtectedRoute>
                 }

43d53c3793b243770ce2bb53866c6d9562536728
Imanuelkarl <appdeveloperemmanuel@gmail.com>
2026-07-31 10:30:48 +0100

    PWA Features

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      // Define manifest, icons, and theme colors here
+      workbox: {
+        // 3 MiB = 3 * 1024 * 1024 bytes
+        maximumFileSizeToCacheInBytes: 3145728,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "My Manpower Project",
+        short_name: "ManPower",
+        description: "My awesome React PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable", // Crucial for mobile layouts
+          },
+        ],
+      },
+    }),
+  ],
+});
